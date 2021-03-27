f27c4f0fbb041e658d98abed27e0bfe0abf4a4a3
linxia25 <linxia25@hotmail.com>
2021-03-26 21:29:07 -0600

    team members

---
+++ seeds/members.js
@@ -0,0 +1,36 @@
+module.exports = [
+  // for each object: add id, title, description, width, height, pathURL, linkURL, credit, creditURL
+    {
+      name:       'Jess Luu',
+      profilePic: 'images/jess.jpg',
+      title:      'Excellent Member',
+      bio:        'Jess is an aspiring writer who solely befriends the local gourmands with connections to the underground truffle ring.',
+      github:     'https://github.com/jluu38'
+    },
+
+    {
+      name:       'Ellie Forte',
+      profilePic: 'images/ellie.jpg',
+      title:      'Cheerleader',
+      bio:        'Confused and unsure but here to cheer.',
+      github:     'https://github.com/elfort2021/'
+    },
+
+    {
+      name:       'Boxiang Xu',
+      profilePic: 'images/boxiang.jpg',
+      title:      'Excellent Member',
+      bio:        'Sometime I feel like a developer, sometimes I don"t"' ,
+      github:     'https://github.com/Togepi-xbx'
+    },
+
+    {
+      name:       'Xia Lin',
+      profilePic: 'images/xia.jpg',
+      title:      'Excellent Member',
+      bio:        'I am pretty and cute!',
+      github:     'https://github.com/linxia25'
+    },
+
+  ];
+
