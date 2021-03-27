c0e00089f2e7a6cc1ee4c7b9b69a0d04a0204a3a
Jess Luu <jessica.luu@edu.sait.ca>
2021-03-26 23:07:19 -0600

    added schema and seed for ingredients and import.js reflects the uploaded info

--- import.js
+++ import.js
@@ -2,11 +2,18 @@ const mongoose = require('./_connection.js')
 
 // Import seed data
 const subSeed = require(`./seeds/subscribers.js`);
+// const ingrSeed = require(`./seeds/ingredients`);
 
 // Define model
 const Subscriber = require(`./models/subscriber.js`);
+// const Ingredient = require(`./models/recipemodel`);
 
 Subscriber.insertMany(subSeed, function(error, subscriber) {
   console.log('Data import completed.')
   mongoose.connection.close();
 });
+
+// Ingredient.insertMany(ingrSeed, function(error, ingredient) {
+//   console.log('Data import completed.')
+//   mongoose.connection.close();
+// });
