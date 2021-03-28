c885caa05fbd3f4f304ad414af4d2e187029d7a9
Jess Luu <jessica.luu@edu.sait.ca>
2021-03-27 22:23:17 -0600

    recipe generator frontend js almost complete; tweaks to ingr seeds to incl serving cover svg; empty 404 ejs added

--- seeds/ingredients.js
+++ seeds/ingredients.js
@@ -1,66 +1,94 @@
 module.exports = [{
     id: 1,
     ingr: "Durian",
-    ingrImage: "",
-    titleTrait: "Yellow"
+    imgSrc: "durian.svg",
+    titleTrait: "Yellow",
+    width: 500,
+    height: 500
   },
   {
     id: 2,
     ingr: "Papaya",
-    ingrImage: "",
-    titleTrait: "Orange"
+    imgSrc: "papaya.svg",
+    titleTrait: "Orange",
+    width: 500,
+    height: 500
   },
   {
     id: 3,
     ingr: "Kiwi",
-    ingrImage: "",
-    titleTrait: "Brown"
+    imgSrc: "kiwi.svg",
+    titleTrait: "Brown",
+    width: 500,
+    height: 500
   },
   {
     id: 4,
     ingr: "Blue Cheese",
-    ingrImage: "",
-    titleTrait: "Blue"
+    imgSrc: "bluecheese.svg",
+    titleTrait: "Blue",
+    width: 500,
+    height: 500
   },
   {
     id: 5,
     ingr: "Chunky Buttermilk",
-    ingrImage: "",
-    titleTrait: "Chunky"
+    imgSrc: "chunkybuttermilk.svg",
+    titleTrait: "Chunky",
+    width: 500,
+    height: 500
   },  {
     id: 6,
     ingr: "Unsalted Butter",
-    ingrImage: "",
-    titleTrait: "Unsalted"
+    imgSrc: "unsaltedbutter.svg",
+    titleTrait: "Unsalted",
+    width: 500,
+    height: 500
   },  {
     id: 7,
     ingr: "Surströmming",
-    ingrImage: "",
-    titleTrait: "Surströmming"
+    imgSrc: "surstromming.svg",
+    titleTrait: "Surströmming",
+    width: 500,
+    height: 500
   },  {
     id: 8,
     ingr: "Preserved Egg",
-    ingrImage: "",
-    titleTrait: "Preserved Egg"
+    imgSrc: "preservedegg.svg",
+    titleTrait: "Preserved Egg",
+    width: 500,
+    height: 500
   },  {
     id: 9,
     ingr: "Kiviak",
-    ingrImage: "",
-    titleTrait: "Kiviak"
+    imgSrc: "kiviak.svg",
+    titleTrait: "Kiviak",
+    width: 500,
+    height: 500
   },  {
     id: 10,
     ingr: "Instant Noodles",
-    ingrImage: "",
-    titleTrait: "Ramen"
+    imgSrc: "instantnoodles.svg",
+    titleTrait: "Ramen",
+    width: 500,
+    height: 500
   },  {
     id: 11,
     ingr: "Cornmeal",
-    ingrImage: "",
-    titleTrait: "Cornbread"
+    imgSrc: "cornmeal.svg",
+    titleTrait: "Cornbread",
+    width: 500,
+    height: 500
   },  {
     id: 12,
     ingr: "Pretzels",
-    ingrImage: "",
-    titleTrait: "Party Mix"
+    imgSrc: "pretzels.svg",
+    titleTrait: "Party Mix",
+    width: 500,
+    height: 500
+  },  {
+    id: 13,
+    ingr: "Serving Cover",
+    imgSrc: "servingcover.svg"
   }
 ]
