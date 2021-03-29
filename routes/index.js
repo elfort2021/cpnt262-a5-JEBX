f1f98786b70a21e66f7f407457d625831636de4a
Boxiang Xu <boxiang.xu@edu.sait.ca>
2021-03-28 23:19:05 -0600

    change images's name & add get(gallery/:id function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const config = require('../config')
+const Recipe = require('../models/recipemodel')
 
 router.use((req, res, next) => {
   res.locals = config
@@ -22,5 +23,26 @@ router.get('/admin', (req, res) => {
 router.get('/gallery', (req, res) => {
   res.render('pages/recipegen', {pageTitle: "Recipe Generator"})
 })
+
+router.get('/gallery/:id', (req, res) => {
+  // use findOne() to find the 'id' in url
+  Recipe.findOne({id: req.params.id}, (err, item) => {        
+    let image = item;
+    // catch error if 'id' is not a number
+    if(err) {
+      res.status(404);
+      res.send('404: Page Not Found');
+    }
+    // returns the single object
+    else {
+      res.render('./pages/single-item', {
+        pageTitle: `Single Image`,
+        alt: '',
+        rec: image
+      });
+    };
+  });
+})
+
 
 module.exports = router;
